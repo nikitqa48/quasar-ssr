689ee01f830fc89c69386fd522df7b363fc3a162
unknown <you@example.com>
2020-09-21 10:34:54 +0300

    event store

--- src/store/event/actions.js
+++ src/store/event/actions.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 export function getEvent ({ commit }) {
-    const url = 'http://127.0.0.1:8000/event_api/'
+    const url = 'https://backendinvest.admlr.lipetsk.ru/event_api/'
         return axios.get(url).then(
             ({ data }) => {
           commit('changeEvent', data)
